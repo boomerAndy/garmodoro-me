f4cb0732f79483ef3521509e18b3d67ad3817e6c
Andrew Jackson <s00198590@mail.itsligo.ie>
2022-05-01 16:36:49 +0100

    Updated SDK_HOME  to dotfile location for makefile

--- properties.mk
+++ properties.mk
@@ -1,4 +1,4 @@
 DEVICE = fr245
-SDK_HOME = /home/maki/connectiq-sdk-manager
+SDK_HOME = /home/maki/.Garmin/ConnectIQ/Sdks/connectiq-sdk-lin-4.1.2-2022-04-07-15e8df3ed
 DEPLOY = /run/media/maki/GARMIN/GARMIN/APPS
 PRIVATE_KEY = /home/maki/projects/connectiq/developer_key
